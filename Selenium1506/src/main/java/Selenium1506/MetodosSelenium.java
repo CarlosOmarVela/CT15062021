516d1d1f97e6187030b479fbddd18a78096c18f3
comar <comar@LAPTOP-4L5M6NIS>
2021-06-16 18:13:36 -0500

    WebElements

--- Selenium1506/src/main/java/Selenium1506/MetodosSelenium.java
+++ Selenium1506/src/main/java/Selenium1506/MetodosSelenium.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MetodosSelenium {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.out.println("Test");
 		//Find the drivers path
@@ -21,6 +21,8 @@ public class MetodosSelenium {
 		WebDriver driver = new ChromeDriver();
 		//Open web page
 		driver.get("https://opensource-demo.orangehrmlive.com");
+		//Maxime the window
+		driver.manage().window().maximize();
 		
 		//Get objects or Web Elements from web page
 		WebElement userName = driver.findElement(By.id("txtUsername"));
@@ -30,8 +32,29 @@ public class MetodosSelenium {
 		userName.sendKeys("Admin");
 		password.sendKeys("admin123");
 		btnLogin.click();
-		driver.close();
-		driver.quit();
+		Thread.sleep(2000);
+		
+		//Explore Method
+		String title = driver.getTitle();
+		System.out.println("El título es: " + title);
+		
+		//Validate homepage
+		String urlDashboard = driver.getCurrentUrl();
+		System.out.println("Current URL is: " + urlDashboard);
+		boolean loginCorrect = urlDashboard.contains("/dashboard");//return boolean
+		if(loginCorrect) {
+			System.out.println("Login correcto");
+		}else {
+			System.out.println("Login incorrecto");
+		}
+		
+		//Navigation method
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
+
+		driver.close(); //Close the current tab
+		driver.quit(); //Close all the windows
 	}
 
 }
